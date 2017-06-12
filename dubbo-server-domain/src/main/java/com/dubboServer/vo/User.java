27f94e0e002b124318f54124db7a9ff2f8e0bf1b
xuliangxiao <liangxiao.xu@eascs.com>
2017-06-12 17:53:58 +0800

    add

---
+++ dubbo-server-domain/src/main/java/com/dubboServer/vo/User.java
@@ -0,0 +1,31 @@
+package com.dubboServer.vo;
+
+/**
+ * Created by xuliangxiao on 2017/6/12 17:52
+ */
+public class User {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
---
+++ dubbo-server-service/src/main/java/com/dubboServer/service/CommonService.java
@@ -0,0 +1,10 @@
+package com.dubboServer.service;
+
+/**
+ * Created by xuliangxiao on 2017/6/12 17:50
+ */
+public interface CommonService {
+
+    String ask(String name);
+
+}
