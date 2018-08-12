fe3ed7145525f150cfceda4ecbe8267cce6f80de
xuliangxiao <liangxiao.xu@eascs.com>
2018-08-12 21:50:17 +0800

    接入sentinel，限制接口QPS

--- dubbo-server-service/src/main/java/com/dubboServer/service/impl/CommonServiceImpl.java
+++ dubbo-server-service/src/main/java/com/dubboServer/service/impl/CommonServiceImpl.java
@@ -1,11 +1,21 @@
 package com.dubboServer.service.impl;
 
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import com.dubboServer.service.CommonService;
 import com.dubboServer.vo.req.UserReq;
 import com.dubboServer.vo.res.UserRes;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by xuliangxiao on 2017/6/13 12:46
@@ -16,10 +26,37 @@ class CommonServiceImpl implements CommonService {
 
     @Override
     public UserRes getUser(UserReq user) {
+        initFlowRules();
+
         log.info("param:" + user.toString());
-        UserRes userRes = new UserRes(user.getName(), 10);
-        log.info("return:" + userRes.toString());
+        UserRes userRes = null;
+        Entry entry = null;
+        try{
+            entry = SphU.entry("index");
+            userRes = new UserRes(user.getName(), 10);
+        }catch( BlockException e ){
+
+        }finally {
+            if( entry!= null ){
+                entry.exit();
+            }
+        }
+        if( userRes!= null ){
+            log.info("return:" + userRes.toString());
+        }
         return userRes;
     }
 
+    /**
+     * 初始化规则
+     */
+    public static void initFlowRules(){
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule rule = new FlowRule();
+        rule.setResource("index");
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS );
+        rule.setCount( 10 );//set limit QPS to 10
+        rules.add(rule);
+        FlowRuleManager.loadRules(rules);
+    }
 }
